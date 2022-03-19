fae6b255703020bb5202964290d17e27a74148fc
cepiek01 <101943609+cepiek01@users.noreply.github.com>
2022-03-19 13:10:23 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿//Grading ID: K1731
+//Lab 1
+//30 August
+//CIS 199-02
+//A Simple Program to print personal information to the console
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Grading ID:     K1731");
+            Console.WriteLine("Hobbies:        Miniature Wargamming and video games");
+            Console.WriteLine("Favorite Book:  Dungeon World");
+            Console.WriteLine("Favorite Movie: John Wick");
+        }
+    }
+}
